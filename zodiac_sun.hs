d60f2f13ed5352208c8d362c39f34f1216a2fd99
Bernadette <bernadetteclermont1@gmail.com>
2025-03-01 08:51:27 -0600

    initial file

---
+++ zodiac_sun.hs
@@ -0,0 +1,7 @@
+
+main :: IO ()
+main = 
+    do 
+        putStrLn "Welcome to Zodiac Sun! Enter your date of birth to discover your Zodiac Sun Sign!"
+        input <- getLine
+        putStrLn "Great!"
