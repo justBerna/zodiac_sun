455c0dcd508dbff33c011bb592af5e2f10fb4ba6
Bernadette <bernadetteclermont1@gmail.com>
2026-01-10 16:09:36 -0500

    initial screen

--- GUI/src/main.js
+++ GUI/src/main.js
@@ -1,9 +1,42 @@
-const {QMainWindow, QLabel} = require('@nodegui/nodegui');
+const {QWidget, QLabel, FlexLayout, QMainWindow, QBoxLayout, Direction} = require('@nodegui/nodegui');
 
-const win = new QMainWindow();
-const helloText = new QLabel();
-helloText.setText('Welcome to Zodiac Sun!');
+function main(){
+    const win = new QMainWindow();
+    win.setFixedSize(500, 150)
+    win.setWindowTitle("Zodiac Sun");
 
-win.show()
+    const rootLayout = new FlexLayout();
 
-global.win = win
+    const mainView = new QWidget();
+    mainView.setObjectName("mainView");
+    mainView.setLayout(rootLayout);
+
+    const welcomeText = new QLabel();
+    welcomeText.setObjectName("welcomeText");
+    welcomeText.setText('Welcome to Zodiac Sun!');
+
+    rootLayout.addWidget(welcomeText);
+
+    win.setCentralWidget(mainView);
+
+    win.setStyleSheet(`
+        #mainView {
+            flex: 1;
+            background-color: 'transparent;
+            justify-content: 'center';
+        }
+
+        #welcomeText {
+            flex: 1;
+            font-size: 16px;
+            font-weight: bold;
+        }
+    `);
+
+    win.show();
+    global.win = win;
+}
+
+main();
+
+
