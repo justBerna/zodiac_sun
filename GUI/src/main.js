5b915988555d11b603690ca7a4e4416069742d5d
Bernadette <bernadetteclermont1@gmail.com>
2026-01-10 17:04:55 -0500

    bare minimum parts added

--- GUI/src/main.js
+++ GUI/src/main.js
@@ -1,4 +1,15 @@
-const {QWidget, QLabel, FlexLayout, QMainWindow, QBoxLayout, Direction} = require('@nodegui/nodegui');
+const {QWidget, QLabel, FlexLayout, QMainWindow, QComboBox, QPushButton} = require('@nodegui/nodegui');
+
+// TODO: Helper Functions: - Figure out how to dynamically pupulate the month & day selectors 
+function populateMonth (){
+
+};
+
+function populateDay () {
+
+}
+
+// Main Function
 
 function main(){
     const win = new QMainWindow();
@@ -15,21 +26,58 @@ function main(){
     welcomeText.setObjectName("welcomeText");
     welcomeText.setText('Welcome to Zodiac Sun!');
 
+    const title = new QLabel();
+    title.setObjectName("mainTitle");
+    title.setText = ("Zodiac Sun");
+
+    const month = new QComboBox();
+    month.setObjectName("monthSelector");
+    month.addItem(undefined, "JAN");
+    month.addItem(undefined, "FEB");
+    month.addItem(undefined, "MAR")
+
+    const day = new QComboBox();
+    day.setObjectName("daySelector");
+    day.addItem(undefined, "1");
+    day.addItem(undefined, "2");
+    day.addItem(undefined, "3");
+
+
+    const submitBtn = new QPushButton();
+    submitBtn.setObjectName("submitBtn");
+    submitBtn.setText("Submit");
+
     rootLayout.addWidget(welcomeText);
+    rootLayout.addWidget(month);
+    rootLayout.addWidget(day);
+    rootLayout.addWidget(submitBtn);
 
     win.setCentralWidget(mainView);
 
     win.setStyleSheet(`
         #mainView {
             flex: 1;
-            background-color: 'transparent;
             justify-content: 'center';
         }
-
+    
         #welcomeText {
             flex: 1;
-            font-size: 16px;
-            font-weight: bold;
+            font-size: '10px';
+            font-weight: 'bold';
+            text-align: 'center';
+        }
+
+        #monthSelector {
+        flex: 0.5;
+        }
+
+        #daySelector {
+        flex: 0.5
+        }
+
+        #submitBtn {
+        flex: 2;
+        background-color:rgb(44, 163, 200);
         }
     `);
 
