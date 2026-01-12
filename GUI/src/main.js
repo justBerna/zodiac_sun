35d4c0c802f9d38698b54eea02f525a53321933e
Bernadette <bernadetteclermont1@gmail.com>
2026-01-12 11:01:59 -0500

    help icon + basic layout structure

--- GUI/src/main.js
+++ GUI/src/main.js
@@ -1,4 +1,4 @@
-const {QWidget, QLabel, FlexLayout, QMainWindow, QComboBox, QPushButton, Direction, QBoxLayout, WindowType, WidgetAttribute, QPixmap, AspectRatioMode} = require('@nodegui/nodegui');
+const {QWidget, QLabel, FlexLayout, QMainWindow, QComboBox, QPushButton, Direction, QBoxLayout, WindowType, WidgetAttribute, QPixmap, AspectRatioMode, QGridLayout} = require('@nodegui/nodegui');
 const path= require("node:path");
 
 // TODO: Helper Functions: - Figure out how to dynamically pupulate the month & day selectors 
@@ -15,7 +15,6 @@ function populateDay () {
 function main(){
     const win = new QMainWindow();
     win.setWindowFlag(WindowType.FramelessWindowHint, true);
-    win.setAttribute(WidgetAttribute.WA_TranslucentBackground, true);
 
     const mainView = new QWidget();
     mainView.setObjectName("mainView");
@@ -29,79 +28,63 @@ function main(){
     welcomeText.setObjectName("welcomeText");
     welcomeText.setText('Zodiac Sun');
 
-    const sun_img = new QPixmap(path.join(__dirname, '../assets/icons8-sun-80.png'));
+    const sun_img = new QPixmap(path.join(__dirname, '../assets/icons8-sun-40.png'));
     const sunIcon = new QLabel(); 
     sunIcon.setObjectName("sunIcon");
     sunIcon.setPixmap(sun_img);
 
-    //const toolTipIcon = new QIcon();
+    const tool_tip_img = new QPixmap(path.join(__dirname, '../assets/icons8-info-50.png'));
+    const toolTipIcon = new QLabel();
+    toolTipIcon.setPixmap(tool_tip_img);
 
-    const titleLayout = new FlexLayout();
-    titleLayout.setFlexNode(win.getFlexNode());
+    const titleLayout = new QGridLayout();
     titleLayout.setObjectName("titleLayout");
-    titleLayout.addWidget(sunIcon);
-    titleLayout.addWidget(welcomeText);
-    //titleLayout.addWidget(toolTipIcon);
+    titleLayout.addWidget(sunIcon, 1, 1);
+    titleLayout.addWidget(welcomeText, 1 ,2);
+    titleLayout.addWidget(toolTipIcon, 1, 3);
 
     rootLayout.addLayout(titleLayout);
 
     // Date Layout
+    const month = new QComboBox();
+    month.setObjectName("monthSelector");
+    month.addItem(undefined, "JAN");
+    month.addItem(undefined, "FEB");
+    month.addItem(undefined, "MAR")
 
-    // const month = new QComboBox();
-    // month.setObjectName("monthSelector");
-    // month.addItem(undefined, "JAN");
-    // month.addItem(undefined, "FEB");
-    // month.addItem(undefined, "MAR")
-
-    // const day = new QComboBox();
-    // day.setObjectName("daySelector");
-    // day.addItem(undefined, "1");
-    // day.addItem(undefined, "2");
-    // day.addItem(undefined, "3");
+    const day = new QComboBox();
+    day.setObjectName("daySelector");
+    day.addItem(undefined, "1");
+    day.addItem(undefined, "2");
+    day.addItem(undefined, "3");
 
 
-    // const dateLabel = new QLabel();
-    // dateLabel.setObjectName("dateLabel");
-    // dateLabel.setText("Date of Birth:");
-    // const dateWidget = new QWidget();
-    // dateWidget.setObjectName("dateWidget");
-    // const dateLayout = new QBoxLayout(Direction.LeftToRight);
-    // dateWidget.setLayout(dateLayout);
-    // dateLayout.addWidget(month);
-    // dateLayout.addWidget(day);
+    const dateLabel = new QLabel();
+    dateLabel.setObjectName("dateLabel");
+    dateLabel.setText("Date of Birth:");
 
-    // const submitBtn = new QPushButton();
-    // submitBtn.setObjectName("submitBtn");
-    // submitBtn.setText("Submit");
+    const dateLayout = new QGridLayout();
+    dateLayout.addWidget(dateLabel, 1, 1);
+    dateLayout.addWidget(month, 2, 1);
+    dateLayout.addWidget(day, 2, 2);
 
-    
-    //rootLayout.addWidget(welcomeText);
-    // rootLayout.addWidget(dateLabel);
-    // rootLayout.addWidget(dateWidget);
-    // rootLayout.addWidget(submitBtn);
+    rootLayout.addLayout(dateLayout);
+
+    const submitBtn = new QPushButton();
+    submitBtn.setObjectName("submitBtn");
+    submitBtn.setText("Submit");
+    rootLayout.addWidget(submitBtn);
 
     win.setCentralWidget(mainView);
 
     win.setStyleSheet(`
         #mainView {
         background-color: rgb(245, 243, 243);
-        }
-
-        #titleLayout {
-        flex: 1;
-        flex-direction: 'column';
         justify-content: 'center';
+        align-content: 'center';
         }
 
-        #sunIcon {
-        flex-shrink: 2;
-        }
-    
-        #welcomeText {
-        flex-grow: 2;
-        text-align: 'center';
-        color: #000000;
-        }
+
     `);
 
     win.show();
